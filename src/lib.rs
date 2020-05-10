341993cb0b83a6003aa8ed0239077487f59b3c03
Akshay Naik <inbox.akshaynaik@gmail.com>
2020-05-10 20:04:15 +0530

    completed minigrep project

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
+use std::env;
 use std::error::Error;
 use std::fs;
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -12,9 +14,14 @@ impl Config {
             return Err("not enough arguments");
         }
 
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
         let config = Config {
-            query: args[1].clone(),
-            filename: args[2].clone(),
+            query,
+            filename,
+            case_sensitive,
         };
 
         Ok(config)
@@ -24,7 +31,13 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string(config.filename)?;
 
-    for line in search(&config.query, &content) {
+    let result = if config.case_sensitive {
+        search(&config.query, &content)
+    } else {
+        search_case_insensitive(&config.query, &content)
+    };
+
+    for line in result {
         println!("{}", line);
     }
 
